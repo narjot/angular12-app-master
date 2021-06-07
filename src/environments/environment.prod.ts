358d4e2509719c66b366770dda0780398ded9b71
narjot <singhnarjot92@gmail.com>
2021-06-07 13:51:59 -0400

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 12.0.2',
+    bootstrap: 'Bootstrap 5.0.1',
+    fontawesome: 'Font Awesome 5.15.3',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 12.0.2',
+    bootstrap: 'Bootstrap 5.0.1',
+    fontawesome: 'Font Awesome 5.15.3',
+  }
+};
